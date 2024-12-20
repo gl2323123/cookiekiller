ff4d8b5ddd15e4cccd89e5ac39df66f0cf27d318
GL~ <gleblobacev07@gmail.com>
2024-12-20 15:46:58 +0300

    Commit2

--- task.js
+++ task.js
@@ -1,11 +1,11 @@
 const cookieImage = document.getElementById("cookie");
 let cookieClick = document.getElementById("clicker__counter");
 
-
 cookieImage.onclick = function () {
     cookieImage.classList.contains('clicker__cookie') ? cookieImage.className = 'new__clicker__cookie' : cookieImage.className = 'clicker__cookie';
 
     let currentClicks = parseInt(cookieClick.textContent);
     cookieClick.textContent = currentClicks + 1;
-
-};
+    cookie.width = 180;
+    setTimeout(() => {cookie.width = 200;}, 50);
+  }
